f15c041ec8ce100c37e30c8e0bc31a13dc52ae34
Eskil Aronsen <eskil.aronsen@gmail.com>
2023-09-26 12:12:37 +0200

    Initial Commit

---
+++ TCA-TreeBasedNavBug/TCA_TreeBasedNavBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_TreeBasedNavBugApp.swift
+//  TCA-TreeBasedNavBug
+//
+//  Created by Eskil Aronsen on 9/26/23.
+//
+
+import SwiftUI
+
+@main
+struct TCA_TreeBasedNavBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
